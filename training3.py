fb50361529f80ae61d7016523ec2feaff804e015
hiroki <functionp@gmail.com>
2017-02-11 22:01:49 +0900

    comment追加

--- training3.py
+++ training3.py
@@ -11,6 +11,14 @@ def multiply_dict(dic, n):
 print(multiply_dict({"Tom" : 90, "Jane" : 43, "Mike" : 32}, 10))
 # >>> {"Tom" : 900, "Jane" : 430, "Mike" : 320}
 
+'''
+正解です。
+
+ディクショナリの内包表記を使うとカッコよく書けます
+
+def multiply_dict(dic, n):
+    return {key: value * n for key, value in dic.items()}
+'''
 
 
 # 問題 2
@@ -27,6 +35,11 @@ def merge_dict(a, b):
 print(merge_dict({"Tom": 90, "Jane": 43, "Mike": 32}, {"Michael": 32, "Alice": 32, "Tom": 32, "Mike": 44}))
 # >>> {"Tom": 122, "Jane": 43, "Mike": 76, "Michael": 32, "Alice": 32}
 
+'''
+正解です
+
+変数名、a, bでなくdict1, dict2とかってわかりやすくつけたいですね
+'''
 
 
 
@@ -42,3 +55,7 @@ def sort_dict_by_value(a):
 
 print(sort_dict_by_value({"Michael": 23, "Alice": 54, "Tom": 39, "Mike": 44}))
 # >>> ["Michael", "Alice", "Tom", "Mike"]
+
+'''
+問題は、valueの値の大きい順に並べ替えてキーのリストを出力、ですよー
+'''
