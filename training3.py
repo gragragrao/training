237829064288e9a252b9c1cd927f6131bd90a7fb
小倉健太郎 <mathbass.ken531@gmail.com>
2017-02-14 21:06:04 +0900

    問題3の修正をしました

--- training3.py
+++ training3.py
@@ -46,16 +46,33 @@ print(merge_dict({"Tom": 90, "Jane": 43, "Mike": 32}, {"Michael": 32, "Alice": 3
 # 問題 3
 
 
-def sort_dict_by_value(a):
-    list = []
-    for key in a:
-        list.append(key)
-    return list
+# ↓↓↓↓↓↓↓↓１回目の回答↓↓↓↓↓↓↓↓
 
-
-print(sort_dict_by_value({"Michael": 23, "Alice": 54, "Tom": 39, "Mike": 44}))
-# >>> ["Michael", "Alice", "Tom", "Mike"]
+# def sort_dict_by_value(a):
+#     list = []
+#     for key in a:
+#         list.append(key)
+#     return list
+#
+#
+# print(sort_dict_by_value({"Michael": 23, "Alice": 54, "Tom": 39, "Mike": 44}))
+# # >>> ["Michael", "Alice", "Tom", "Mike"]
 
 '''
 問題は、valueの値の大きい順に並べ替えてキーのリストを出力、ですよー
 '''
+
+
+# 修正しました（↓↓↓↓↓↓↓２回目の回答↓↓↓↓↓↓）
+
+
+def sort_dict_by_value(dict):
+    dict_rev = {value: key for key, value in dict.items()}
+    list_dict_rev_key = [key for key in dict_rev]
+    # return list_dict_rev_key # >>> [23, 54, 39, 44]
+    list_dict_rev_key.sort(reverse = True)
+    # return list_dict_rev_key # >>> [54, 44, 39, 23]
+    return [dict_rev[key] for key in list_dict_rev_key]
+
+
+print(sort_dict_by_value({"Michael": 23, "Alice": 54, "Tom": 39, "Mike": 44}))
